d41942b151d5d97f0ce9e8d1bb0142bf5aafa360
keshav304 <keshavjhaa2678@gmail.com>
2022-09-13 18:38:55 +0530

    quick fix

--- pages/demo-page.jsx
+++ pages/demo-page.jsx
@@ -38,29 +38,29 @@ export default function Demo(props) {
       setTags(updatedTags);
     }
   };
-  const listenScrollEvent = e => {
+  const listenScrollEvent = (e) => {
     if (window.scrollY < 125 && window.isOpen) {
-      document.querySelector('.sidebarIcon').style.marginLeft = '20.6%'
+      document.querySelector('.sidebarIcon').style.marginLeft = '20.6%';
     }
     if (window.scrollY < 125 && !window.isOpen) {
-      document.querySelector('.sidebarIcon').style.marginLeft = '17.6%'
+      document.querySelector('.sidebarIcon').style.marginLeft = '17.6%';
     }
     if (window.scrollY > 125 && !window.isOpen) {
-      document.querySelector('.sidebarIcon').style.marginLeft = '0.6%'
+      document.querySelector('.sidebarIcon').style.marginLeft = '0.6%';
     }
-  }
-  useEffect(()=>{
-    window.addEventListener('scroll', listenScrollEvent)
-  },[])
-  useEffect(()=>{
-    window.isOpen=isOpen
-  if (isOpen && window.scrollY < 125) {
-    document.querySelector('.sidebarIcon').style.marginLeft='20.6%'
-  } 
-  if (!isOpen && window.scrollY < 125) {
-    document.querySelector('.sidebarIcon').style.marginLeft="17.6%"
-  }
-  },[isOpen])
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', listenScrollEvent);
+  }, []);
+  useEffect(() => {
+    window.isOpen = isOpen;
+    if (isOpen && window.scrollY < 125) {
+      document.querySelector('.sidebarIcon').style.marginLeft = '20.6%';
+    }
+    if (!isOpen && window.scrollY < 125) {
+      document.querySelector('.sidebarIcon').style.marginLeft = "17.6%";
+    }
+  }, [isOpen]);
   async function fetchData() {
     try {
       console.info('fetching live preview data...');
@@ -87,14 +87,17 @@ export default function Demo(props) {
       footer={getFooter}
       page={result}
     >
-            <div className="sidebarIcon">
-        <img src='https://i.imgur.com/CnW2LF0.png' onClick={()=>setIsOpen(!isOpen)}/>
+      <div className="sidebarIcon">
+        <img src="https://i.imgur.com/CnW2LF0.png" onClick={() => setIsOpen(!isOpen)} />
         <h2>Home</h2>
-        </div>
-          <div className="homeInfoContainer">
+      </div>
+      <div className="homeInfoContainer">
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Pellentesque in feugiat nulla. Integer a augue ut leo cursus convallis.</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Pellentesque in feugiat nulla. Integer a augue ut leo cursus convallis.
+
+        </p>
       </div>
       <SliderSidebar
         isSidebarOpen={isOpen}
