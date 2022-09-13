85ffb3c89bb74c7253cd00af6a993826fa8aed5b
keshav304 <keshavjhaa2678@gmail.com>
2022-09-13 15:54:24 +0530

    quick fix

--- components/category-section.js
+++ components/category-section.js
@@ -46,9 +46,9 @@ function CategorySection(props) {
   // const { _metadata: { uid } } = props;
   // console.log(props,"xxx")
   return (
-    <div className={props.props.heading === "Shop By Room" ? "leftCategorycontainer" : "rightCategorycontainer"}>
-      {props.props.heading ? <h2 className={props.props.heading === "Shop By Room" ? "leftCategoryHeading" : "rightCategoryHeading"}>{props.props.heading}</h2> : null}
-      <ul className={props.props.heading === "Shop By Room" ? "leftCategories" : "rightCategories"}>
+    <div className={props.props.heading === "Home" ? "leftCategorycontainer" : "rightCategorycontainer"}>
+      {props.props.heading ? <Link href="/demo-page"><h2 className={props.props.heading === "Home" ? "leftCategoryHeading" : "rightCategoryHeading"}>{props.props.heading}</h2></Link> : null}
+      <ul className={props.props.heading === "Home" ? "leftCategories" : "rightCategories"}>
         {
           // eslint-disable-next-line max-len
           categories
